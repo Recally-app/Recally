f0ffba20ed2a09bee56100d9b9e0cfaa80a75fe9
Ovietobore Oghre <ooghre@yahoo.com>
2025-10-25 20:06:17 -0400

    small fixes here

--- extension/src/popup/main.tsx
+++ extension/src/popup/main.tsx
@@ -1,10 +1,11 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import PopUpApp from './popup';
-import '../styles/index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <PopUpApp />
-    </StrictMode>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
