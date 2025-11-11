c8ee2e0f5fd299e3cb05d70a8b87cd68c9136d86
Ovietobore Oghre <ooghre@yahoo.com>
2025-11-11 18:00:24 -0500

    add delete all button

--- shared/storage/types.ts
+++ shared/storage/types.ts
@@ -6,8 +6,14 @@ export interface SavePostResult {
 }
 
 export interface StorageProvider {
-    savePost(url: string, title: string, tags?: string[], faviconUrl?: string): Promise<SavePostResult>;
+    savePost(
+        url: string,
+        title: string,
+        tags?: string[],
+        faviconUrl?: string
+    ): Promise<SavePostResult>;
     getAllPosts(): Promise<Post[]>;
     deletePost(id: string): Promise<void>;
+    deleteAllPosts(): Promise<void>;
     searchPosts(query: string): Promise<Post[]>;
 }
